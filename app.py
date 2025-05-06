f3e512f77363ee038dd2496aae01a94d6ae162e7
ericpratamatan <137317272+ericpratamatan@users.noreply.github.com>
2025-05-06 10:13:54 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from textblob import TextBlob
+
+st.title("Simple Sentiment Analysis")
+
+text = st.text_area("Enter a sentence or review:")
+if st.button("Analyze"):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+
+    if polarity > 0:
+        st.success("Sentiment: Positive ☺️")
+    elif polarity < 0:
+        st.error("Sentiment: Negative 😠")
+    else:
+        st.info("Sentiment: Neutral 😐")
